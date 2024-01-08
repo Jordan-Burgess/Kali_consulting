973f0990a8c82d277abcb1b3837bcc514a3d802f
Jordan-Burgess <alfredjordan93@gmail.com>
2024-01-08 17:17:26 -0500

    Express Boiler Plate Server App

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.get('/', (req, res)=>{
+    res.send('Kali Consulting API')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on Port ${PORT}`)
+})
