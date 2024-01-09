8edd4a0187060b8fa2ab0f21c28ee0dbdf0988b2
ronaldngounou <landryzinkoue@yahoo.fr>
2024-01-09 09:21:50 -0500

    add savedServices on GeneralProfile

--- Models/GeneralProfile.js
+++ Models/GeneralProfile.js
@@ -19,7 +19,11 @@ const userProfileSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: User
-    }
+    },
+    savedServices: [{
+        type: mongooose.Schema.Types.ObjectId,
+        ref: Services
+    }]
 
 })
 
