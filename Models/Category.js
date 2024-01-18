70bef7b2b3cf5bd65d2f24a6f3acda1b3cc484da
ronaldngounou <landryzinkoue@yahoo.fr>
2024-01-18 10:19:16 -0500

    resolve circular depencies between Category and Services

--- Models/Category.js
+++ Models/Category.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose')
-const Service = require('./Service')
 
 const categorySchema = new mongoose.Schema({
     name: String,
     photo: String,
-    services: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: Service
-    }],
-    subcategories: [{
+    subcategories: {
         type: String
-    }]
+    }
 })
 
 const Category = mongoose.model('Category', categorySchema)
